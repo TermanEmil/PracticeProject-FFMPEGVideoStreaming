b452129c8e95903ec78ad04f0cc5732ed500fed6
andrewskx <faithless.user@gmail.com>
2018-07-10 18:56:31 +0300

    fixed merges

--- VideoStreamer/Startup.cs
+++ VideoStreamer/Startup.cs
@@ -28,15 +28,9 @@ namespace VideoStreamer
         {
 
             watcher = new FileSystemWatcher();
-<<<<<<< HEAD
-            watcher.Filter = "*.json";
-            watcher.Changed += new FileSystemEventHandler(OnChanged);
-            watcher.Path = "/Users/andrewska/Desktop/PracticeProject-FFMPEGVideoStreaming/VideoStreamer/";
-=======
             watcher.Filter = "*Channels.json";
             watcher.Changed += new FileSystemEventHandler(OnChanged);
 			watcher.Path = ".";
->>>>>>> e9fc30b707c0c4e76a818e6d36548d99897252d9
             watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
            | NotifyFilters.FileName | NotifyFilters.DirectoryName;
             watcher.EnableRaisingEvents = true;
@@ -55,7 +49,6 @@ namespace VideoStreamer
                 Task.Run(
                     () => procManager.StartChunking(ffmpegConfig, streamCfg));
             }
-<<<<<<< HEAD
         }
 
         private static void OnChanged(object source, FileSystemEventArgs e)
@@ -65,17 +58,6 @@ namespace VideoStreamer
             ChannelUpdate.AddChannel();
         }
 
-=======
-        }
-
-        private static void OnChanged(object source, FileSystemEventArgs e)
-        {
-            // Specify what is done when a file is changed, created, or deleted.
-            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
-            ChannelUpdate.AddChannel();
-        }
-
->>>>>>> e9fc30b707c0c4e76a818e6d36548d99897252d9
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -97,12 +79,8 @@ namespace VideoStreamer
 
         public void Configure(
             IApplicationBuilder app,
-<<<<<<< HEAD
-            IHostingEnvironment env)
-=======
             IHostingEnvironment env,
 			ILoggerFactory loggerFactory)
->>>>>>> e9fc30b707c0c4e76a818e6d36548d99897252d9
         {
             if (env.IsDevelopment())
             {

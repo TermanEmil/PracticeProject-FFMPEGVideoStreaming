31dd6bf716019bcf4a5490d354ee033f26ffbf1e
Emil Terman <terman.emil@gmail.com>
2018-07-10 15:39:23 +0300

    started working on logging

--- VideoStreamer/Controllers/ProcessMonitorController.cs
+++ VideoStreamer/Controllers/ProcessMonitorController.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FFMPEGStreamingTools;
+using Microsoft.AspNetCore.Mvc;
 using VideoStreamer.Models.LivePlayerViewModels;
-using FFMPEGStreamingTools;
-using System.Diagnostics;
-using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
-using System.Net;
 
 namespace VideoStreamer.Controllers
 {
-    [Route("api/[controller]")]
+	[Route("api/[controller]")]
     public class ProcessMonitorController : Controller
     {
         public IActionResult Index()

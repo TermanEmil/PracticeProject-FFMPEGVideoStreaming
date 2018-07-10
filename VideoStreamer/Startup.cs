e9fc30b707c0c4e76a818e6d36548d99897252d9
Emil Terman <terman.emil@gmail.com>
2018-07-10 18:26:37 +0300

    fixed the dynamic channeling

--- VideoStreamer/Startup.cs
+++ VideoStreamer/Startup.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using FFMPEGStreamingTools;
 using FFMPEGStreamingTools.M3u8Generators;
-using FFMPEGStreamingTools.TokenBrokers;
 using FFMPEGStreamingTools.Utils;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -14,60 +14,83 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using VideoStreamer.DB;
 using VideoStreamer.Utils;
+using Serilog.Extensions.Logging;
 
 namespace VideoStreamer
 {
-	public class Startup
+    public class Startup
     {
-		public IConfiguration Cfg { get; }
-		private StreamingProcManager procManager;
+        public IConfiguration Cfg { get; }
+        private StreamingProcManager procManager;
+        public static FileSystemWatcher watcher;
 
-		public Startup(IConfiguration cfg)
+        public Startup(IConfiguration cfg)
         {
-			Cfg = cfg;
-			FFMPEGConfigLoader.Load(
-				out var ffmpegConfig,
-				out var streamsConfig
-			);
 
-			procManager = new StreamingProcManager();         
-			foreach (var streamCfg in streamsConfig)
-			{
-				Task.Run(
-					() => procManager.StartChunking(ffmpegConfig, streamCfg));
-			}
+            watcher = new FileSystemWatcher();
+            watcher.Filter = "*Channels.json";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+			watcher.Path = ".";
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+           | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("File -> {0}", watcher.Path);
+
+
+            Cfg = cfg;
+            FFMPEGConfigLoader.Load(
+                out var ffmpegConfig,
+                out var streamsConfig
+            );
+
+            procManager = new StreamingProcManager();
+            foreach (var streamCfg in streamsConfig)
+            {
+                Task.Run(
+                    () => procManager.StartChunking(ffmpegConfig, streamCfg));
+            }
         }
-        
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            ChannelUpdate.AddChannel();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            
-			var connectionStr = Cfg["DBConnectionStr"];
-			services.AddDbContext<StreamerContext>(
-				o => o.UseSqlite(connectionStr));
 
-			services.AddDistributedRedisCache(o =>
-			{
-				o.Configuration = "localhost";
-				o.InstanceName = "VideoStreamings";
-			});         
+            var connectionStr = Cfg["DBConnectionStr"];
+            services.AddDbContext<StreamerContext>(
+                o => o.UseSqlite(connectionStr));
+
+            services.AddDistributedRedisCache(o =>
+            {
+                o.Configuration = "localhost";
+                o.InstanceName = "VideoStreamings";
+            });
 
             // Custom stuff
-			services.AddTransient<IM3U8Generator, M3U8GeneratorDefault>();
-			services.AddTransient<ITokenBroker, SHA256TokenBroker>();
+            services.AddTransient<IM3U8Generator, M3U8GeneratorDefault>();
+
         }
-  
+
         public void Configure(
-			IApplicationBuilder app,
-            IHostingEnvironment env)
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+			ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+			loggerFactory.AddFile("Logs/ts-{Date}.txt");
+
             app.UseMvc();
-            app.UseStaticFiles();         
+            app.UseStaticFiles();
         }
     }
 }
